c9fdf3673cc7782ae5271d61d9a2b36b72b43605
Nouman Khan <noumanijaz.dev@outlook.com>
2021-11-23 15:24:45 +0500

    CreateContext and modifcation of project

--- expanse-tracker/src/App.js
+++ expanse-tracker/src/App.js
@@ -1,10 +1,22 @@
 import React from 'react'
+import AddTransaction from './components/AddTransaction'
+import Balance from './components/Balance'
+import Header from './components/Header'
+import IncomeExpanses from './components/IncomeExpanses'
+import TransactionList from './components/TransactionList'
+import { GlobalProvider } from './context/GlobalState'
 
 function App() {
     return (
-        <div>
-             <h1>hello world</h1>
-        </div>
+        <GlobalProvider>
+            <Header />
+            <div className="container">
+                <Balance />
+                <IncomeExpanses />
+                <TransactionList />
+                <AddTransaction/>
+            </div>
+        </GlobalProvider>
     )
 }
 
